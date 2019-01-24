cd4d049f588e96cdd0c99680986e6009da882269
superAme <beatamez@hotmail.com>
2019-01-23 20:46:40 -0600

    Calendario Final

--- eventos.php
+++ eventos.php
@@ -17,10 +17,33 @@
 			echo json_encode($result);
 			break;
 		case 'eliminar':
-			echo "eliminar";
+			$respuesta = false;
+			if(isset($_POST['id'])){
+				$sql = $pdo->prepare("DELETE FROM eventos WHERE ID = :ID");
+				$respuesta = $sql->execute(array("ID"=>$_POST['id']));
+			}
+			echo json_encode($respuesta);
 			break;
 		case 'modificar';
-			echo "modificar";
+			
+			$sql = $pdo->prepare('UPDATE eventos SET 
+				titulo=:titulo,
+				descripcion=:descripcion,
+				color=:color,
+				textColor=:textColor,
+				start=:start,
+				end=:end
+				WHERE ID = :ID');
+				$result = $sql->execute(array(
+				"ID" => $_POST['id'],
+				"titulo" => $_POST['titulo'],
+				"descripcion" => $_POST['descripcion'],
+				"color" => $_POST['color'],
+				"textColor" => $_POST['textColor'],
+				"start" => $_POST['start'],
+				"end" => $_POST['end']
+			));
+			echo json_encode($result);
 			break;
 		default:
 			$sql = $pdo->prepare("SELECT * FROM eventos");
